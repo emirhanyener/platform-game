cdb97b7054f4e5170996f648ec2356a25ce5d376
Emirhan Yener <yeneremirhann@gmail.com>
2022-05-30 12:31:55 +0300

    update map

--- src/game/Game.java
+++ src/game/Game.java
@@ -17,15 +17,20 @@ public class Game extends JFrame implements Runnable{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		player = new Player(new Position(50, 300), new Dimension(30, 30));
 		player.setColor(new Color(0, 100, 200));
-		player.moveSpeed.setSpeed(6);
+		player.moveSpeed.setSpeed(8);
 		player.jumpSpeed.setSpeed(4.5f);
 		objects.add(new Object(new Position(0, 500), new Dimension(Setting.WINDOW_WIDTH, Setting.WINDOW_HEIGHT), new Color(0, 255, 0)));
 		objects.add(new Object(new Position(300, 450), new Dimension(100, 50), new Color(100, 40, 0)));
+		objects.add(new Object(new Position(500, 390), new Dimension(100, 50), new Color(120, 35, 0)));
+		objects.add(new Object(new Position(0, 290), new Dimension(450, 10), new Color(140, 30, 0)));
+		objects.add(new Object(new Position(0, 190), new Dimension(50, 5), new Color(160, 25, 0)));
+		objects.add(new Object(new Position(0, 90), new Dimension(800, 5), new Color(180, 20, 0)));
+		objects.add(new Object(new Position(900, 300), new Dimension(50, 15), new Color(200, 15, 0)));
 		addKeyListener(new PlayerControl(this.player));
 		show();
 		
 		Thread th1 = new Thread(this);
-		Physics p1 = new Physics(this.objects, this.player);
+		new Physics(this.objects, this.player);
 		th1.start();
 	}
 	
