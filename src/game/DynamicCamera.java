5183c9921d1ba75f585eadd903b56d10085c5649
Emirhan Yener <yeneremirhann@gmail.com>
2022-06-02 23:22:15 +0300

    fix optimization

--- src/game/DynamicCamera.java
+++ src/game/DynamicCamera.java
@@ -9,19 +9,14 @@ public class DynamicCamera extends Camera{
 		this.target = target;
 		this.firstPosition = new Position(this.target.position.getX(), this.target.position.getY());
 		this.offset = offset;
-		
-		Thread updatePos = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while(true) {
-					position = Position.diff(target.position, firstPosition);
-					position.addX(-Setting.WINDOW_WIDTH / 2 + target.dimension.getWidth());
-					position.addX(offset.getX());
-					position.addY(offset.getY());
-				}
-			}
-		});
-		
-		updatePos.start();
+	}
+	
+	@Override
+	public Position getPosition() {
+		position = Position.diff(target.position, firstPosition);
+		position.addX(-Setting.WINDOW_WIDTH / 2 + target.dimension.getWidth());
+		position.addX(offset.getX());
+		position.addY(offset.getY());
+		return super.getPosition();
 	}
 }
