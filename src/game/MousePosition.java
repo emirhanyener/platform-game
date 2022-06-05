7812f24ab794c00950ba56878ee3cd9c6c8853f1
Emirhan Yener <yeneremirhann@gmail.com>
2022-06-05 12:01:37 +0300

    add mouse position class

---
+++ src/game/MousePosition.java
@@ -0,0 +1,59 @@
+package game;
+
+import java.awt.MouseInfo;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MousePosition implements MouseListener{
+	private static Position position = null;
+	private static MousePosition mousePosition = null;
+	private static Camera camera = null;
+	private MousePosition() {
+		position = new Position();
+	}
+	
+	public static MousePosition getInstance() {
+		if(mousePosition == null)
+			mousePosition = new MousePosition();
+		return mousePosition;
+	}
+	
+	public void setCamera(Camera _camera) {
+		camera = _camera;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		this.position.setX(camera.getRawPosition().getX() + e.getX());
+		this.position.setY(camera.getRawPosition().getY() + e.getY());
+		Alert.getInstance().addAlert("x = " + this.position.getX() + ", y = " + this.position.getY());
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
