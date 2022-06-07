0b383e10c2e4af919401adb903c9905a1dd103c4
Emirhan Yener <yeneremirhann@gmail.com>
2022-06-07 11:10:52 +0300

    add factory

---
+++ src/game/CharacterFactory.java
@@ -0,0 +1,22 @@
+package game;
+
+import java.awt.Color;
+import java.util.LinkedList;
+
+public class CharacterFactory extends Factory{
+	public LinkedList<Player> characters;
+
+	@Override
+	public void createProducts() {
+		characters = new LinkedList<Player>();
+		Player player1 = new Player(new Position(50, 300), new Dimension(30, 30));
+		player1.setColor(new Color(0, 100, 200));
+		player1.moveSpeed.setSpeed(6);
+		player1.jumpSpeed.setSpeed(16f);
+		characters.add(player1);
+	}
+	
+	public LinkedList<Player> getProducts() {
+		return this.characters;
+	}
+}
---
+++ src/game/ObjectFactory.java
@@ -0,0 +1,32 @@
+package game;
+
+import java.awt.Color;
+import java.util.LinkedList;
+
+public class ObjectFactory extends Factory{
+	public LinkedList<Object> objects;
+
+	@Override
+	public void createProducts() {
+		objects = new LinkedList<Object>();
+		objects.add(new Object(new Position(0, 500), new Dimension(Setting.WINDOW_WIDTH, Setting.WINDOW_HEIGHT), new Color(0, 255, 0)));
+		objects.add(new Object(new Position(300, 450), new Dimension(100, 50), new Color(100, 40, 0)));
+		objects.add(new Object(new Position(500, 390), new Dimension(100, 50), new Color(120, 35, 0)));
+		objects.add(new Object(new Position(0, 290), new Dimension(450, 10), new Color(140, 30, 0)));
+		objects.add(new Object(new Position(0, 190), new Dimension(50, 5), new Color(160, 25, 0)));
+		objects.add(new Object(new Position(0, 90), new Dimension(800, 5), new Color(180, 20, 0)));
+		objects.add(new Object(new Position(900, 300), new Dimension(50, 15), new Color(200, 15, 0)));
+		Object object = new Object(new Position(700, 450), new Dimension(100, 50), new Color(0, 200, 10), true);
+		object.SetTriggerEvent(new TriggerEvent() {
+			@Override
+			public void OnTriggered() {
+				object.setColor(Color.ORANGE);
+			}
+		});
+		objects.add(object);
+	}
+
+	public LinkedList<Object> getProducts() {
+		return this.objects;
+	}
+}
