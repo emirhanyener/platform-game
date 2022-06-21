82f4e8625c431d0b804e313eb493329f0312e0b2
Emirhan Yener <yeneremirhann@gmail.com>
2022-06-21 21:48:58 +0300

    fix gameplay

--- src/util/Physics.java
+++ src/util/Physics.java
@@ -26,18 +26,16 @@ public class Physics extends Thread implements Runnable{
 				e.printStackTrace();
 			}
 			for(Object item : objects) {
-				if(item.isAbstract())
-					if(character.position.getX() + character.dimension.getWidth() + 1 > item.position.getX() && character.position.getX() - 5 < item.position.getX() + item.dimension.getWidth() && character.position.getY() + character.velocity.getY() + character.dimension.getHeight() > item.position.getY() && character.position.getY() + character.velocity.getY() < item.position.getY() + item.dimension.getHeight()) {
-						item.Triggered(character);
-						continue;
-					}
 				if(character.position.getX() + character.dimension.getWidth() > item.position.getX() && character.position.getX() < item.position.getX() + item.dimension.getWidth() && character.position.getY() + character.velocity.getY() + character.dimension.getHeight() > item.position.getY() && character.position.getY() + character.velocity.getY() < item.position.getY() + item.dimension.getHeight())
 				{	
-					if(character.position.getY() + character.velocity.getY() < item.position.getY() && !character.down)
-						character.position.setY(item.position.getY() - character.dimension.getHeight());
-					character.velocity.setY(0);
-					if(character.jump) {
-						character.velocity.setY(-character.jumpSpeed.getSpeed());
+					if(!item.isAbstract()) {
+						if(character.position.getY() + character.velocity.getY() < item.position.getY() && !character.down) {
+							character.position.setY(item.position.getY() - character.dimension.getHeight());
+						}
+						character.velocity.setY(0);
+						if(character.jump) {
+							character.velocity.setY(-character.jumpSpeed.getSpeed());
+						}
 					}
 				}
 				else {
@@ -47,14 +45,26 @@ public class Physics extends Thread implements Runnable{
 				
 				if(character.position.getY() + character.dimension.getHeight() - 1 > item.position.getY())
 					if(character.position.getY() < item.position.getY() + item.dimension.getHeight()) {
-						if(Math.abs((character.position.getX() + character.dimension.getWidth() + character.velocity.getX()) - item.position.getX()) < character.moveSpeed.getSpeed())
-							character.position.setX(character.position.getX() - ((character.position.getX() + character.dimension.getWidth() + character.velocity.getX()) - item.position.getX()));
+						if(Math.abs((character.position.getX() + character.dimension.getWidth() + character.velocity.getX()) - item.position.getX()) < character.moveSpeed.getSpeed()) {
+							character.position.setX(item.position.getX() - character.dimension.getWidth());
+						}
+						if(Math.abs((character.position.getX() + character.velocity.getX() - (item.position.getX() + item.dimension.getWidth()))) < character.moveSpeed.getSpeed()) {
+							character.position.setX(item.position.getX() + item.dimension.getWidth());
+						}
 						
-						if(character.position.getX() + character.dimension.getWidth() + character.velocity.getX() > item.position.getX())
-							if(character.position.getX() + character.velocity.getX() < item.position.getX() + item.dimension.getWidth())
-								isMovable = false;
+						if(character.position.getX() + character.dimension.getWidth() + character.velocity.getX() > item.position.getX()) {
+							if(character.position.getX() + character.velocity.getX() < item.position.getX() + item.dimension.getWidth()) {
+								if(item.isAbstract()) {	
+									item.Triggered(character);
+									continue;
+								}
+								else
+									isMovable = false;
+							}
+						}
 					}
 			}
+			
 			if(isMovable)
 				character.position.setX(character.position.getX() + character.velocity.getX());
 			isMovable = true;
